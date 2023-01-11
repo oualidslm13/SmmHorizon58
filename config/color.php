5728ba8eae8857c8ee53d835208cab74c7e7b9e4
oualidslm13 <122484029+oualidslm13@users.noreply.github.com>
2023-01-11 12:02:34 -0800

    smm

---
+++ config/color.php
@@ -0,0 +1,9 @@
+<?php return array (
+    'primaryColor' => 'a460f2',
+    'subheading' => '204dcc',
+    'bggrdleft' => '7c35ff',
+    'bggrdright' => '5900ff',
+    'bggrdleft2' => '8340ff',
+    'btngrdleft' => 'af61f5',
+    'copyrights' => '1d43db',
+);
